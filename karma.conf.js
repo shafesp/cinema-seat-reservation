0ba2b35d982966afd388bc067d0bdedea0e5fc11
shafe pahochiya <65221883+shafesp@users.noreply.github.com>
2022-10-04 15:13:59 +0530

    z

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/car-parking'),
+      dir: require('path').join(__dirname, './coverage/seat-reservation'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'car-parking'`, () => {
+  it(`should have as title 'seat-reservation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('car-parking');
+    expect(app.title).toEqual('seat-reservation');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('car-parking app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('seat-reservation app is running!');
   });
 });
